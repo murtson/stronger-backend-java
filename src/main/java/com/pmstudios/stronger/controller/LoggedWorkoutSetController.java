0592c913bc1d0aaa556eaf15022a5a62981c8b56
Martin Mosesson <martin.mosesson@hiq.se>
2022-10-05 15:49:58 +0200

    fix post request for creating LoggedWorkoutSet

--- src/main/java/com/pmstudios/stronger/controller/LoggedWorkoutSetController.java
+++ src/main/java/com/pmstudios/stronger/controller/LoggedWorkoutSetController.java
@@ -3,6 +3,7 @@ package com.pmstudios.stronger.controller;
 import com.pmstudios.stronger.entity.LoggedWorkoutSet;
 import com.pmstudios.stronger.service.LoggedWorkoutSetService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +15,9 @@ public class LoggedWorkoutSetController {
     LoggedWorkoutSetService loggedWorkoutSetService;
 
     @PostMapping("/user-workout/${workoutId}")
-    ResponseEntity<LoggedWorkoutSet> createLoggedWorkoutSet(@PathVariable Long workoutId) {
-        loggedWorkoutSetService.saveLoggedWorkoutSet()
+    ResponseEntity<LoggedWorkoutSet> createLoggedWorkoutSet(@RequestBody LoggedWorkoutSet loggedWorkoutSet, @PathVariable Long workoutId) {
+        LoggedWorkoutSet createdLoggedWorkoutSet = loggedWorkoutSetService.saveLoggedWorkoutSet(loggedWorkoutSet, workoutId);
+        return new ResponseEntity<>(createdLoggedWorkoutSet, HttpStatus.CREATED);
     }
 
 
